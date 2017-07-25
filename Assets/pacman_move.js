e395ce4c7dd811e5d0401a1a1cd92c89147f1e0f
huangbofang <hbf@haohandata.com.cn>
2017-07-25 17:43:43 +0800

    first commit

---
+++ Assets/pacman_move.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var speed = 0.4;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+
+function FixedUpdate () {
+
+}
+
+function valid(dir) {
+    // Cast Line from 'next to Pac-Man' to 'Pac-Man'
+	var pos = transform.position;
+    RaycastHit2D hit = Physics2D.Linecast(pos + dir, pos);
+    return (hit.collider == collider2D);
+}
